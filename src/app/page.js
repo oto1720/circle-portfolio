199d958f35fc09b10238de0a4b928e0280a406a0
oto1720 <kotaro17206@icloud.com>
2025-04-02 00:10:23 +0900

    初期コミット

---
+++ src/app/page.js
@@ -0,0 +1,18 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>サークル作品集へようこそ！</h1>
+        <p>みんなの作品を共有しましょう</p>
+      </div>
+    );
+  }
+  
+  // src/app/about/page.js - Aboutページ (URL: /about)
+  export default function About() {
+    return (
+      <div>
+        <h1>サイトについて</h1>
+        <p>このサイトは制作サークルの作品を共有するためのものです</p>
+      </div>
+    );
+  }
